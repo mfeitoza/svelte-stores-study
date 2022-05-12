b15decf754c1a06ac9cb57ed6f4fcf28e4c01ef4
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2022-05-11 23:23:51 -0300

    feat(services): add get uses page

--- src/lib/services/api.ts
+++ src/lib/services/api.ts
@@ -17,7 +17,24 @@ interface RGetUsers {
   data: IUser[]
 }
 
+export interface IMeta {
+  page: number
+  per_page: number
+  total: number
+  total_pages: number
+}
+
+interface IUsers {
+  data: User[]
+  meta: IMeta
+}
+
 export async function getUsers(): Promise<User[]> {
   const { data } = await api.get('users?delay=1.5').json<RGetUsers>()
   return data.map((user) => new User(user))
+}
+
+export async function getUsersPage(page = 1): Promise<IUsers> {
+  const { data, ...meta } = await api.get(`users?delay=1.5&page=${page}`).json<RGetUsers>()
+  return { data: data.map((user) => new User(user)), meta }
 }
