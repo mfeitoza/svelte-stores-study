2c67ef1d5dd7eb0710492da4a146246bc518322d
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2022-05-14 01:52:34 -0300

    feat(stores): add elf

---
+++ src/lib/stores/users-elf.ts
@@ -0,0 +1,105 @@
+import type { User } from '$lib/models/user'
+
+import { createStore, withProps, select } from '@ngneat/elf'
+import { createEffect, ofType, createAction, initEffects, registerEffects } from '@ngneat/effects'
+
+import { from, iif, EMPTY, zip, combineLatest, forkJoin } from 'rxjs'
+import { tap, map, mergeMap, switchMap, first, take } from 'rxjs/operators'
+import { getUsersPage, type IMeta } from '$lib/services/api'
+
+interface UsersProps {
+	loaded: boolean
+	users: User[]
+	meta: IMeta
+}
+
+const usersStore = createStore(
+	{ name: 'users' },
+	withProps<UsersProps>({
+		loaded: false,
+		users: [],
+		meta: {
+			page: 1,
+			per_page: 0,
+			total: 0,
+			total_pages: 0
+		}
+	})
+)
+
+initEffects()
+
+export const users$ = usersStore.pipe(select((state) => state.users))
+export const loaded$ = usersStore.pipe(select((state) => state.loaded))
+export const meta$ = usersStore.pipe(select((state) => state.meta))
+export const hasPrev$ = meta$.pipe(
+	select((state) => state.page),
+	map((page) => page > 1)
+)
+export const hasNext$ = meta$.pipe(
+	select((state) => state),
+	map((state) => state.page < state.total_pages)
+)
+
+function addUsers({ data, meta }: { data: User[]; meta: IMeta }) {
+	usersStore.update((_) => ({
+		users: data,
+		loaded: true,
+		meta: meta
+	}))
+}
+
+export function fetchUsers() {
+	return from(getUsersPage()).pipe(tap(addUsers))
+}
+
+export const fetchPrevAction = createAction('fetchPrev')
+export const fetchNextAction = createAction('fetchNext')
+
+export function handlePrev2() {
+	return zip(meta$, hasPrev$)
+		.pipe(
+			mergeMap(([meta, hasPrev]) => iif(() => hasPrev, from(getUsersPage(meta.page - 1)), EMPTY)),
+			tap(console.log),
+			tap(addUsers)
+		)
+		.subscribe()
+}
+
+export function handleNext2() {
+	return zip(meta$, hasNext$).pipe(
+		mergeMap(([meta, hasNext]) => iif(() => hasNext, from(getUsersPage(meta.page + 1)), EMPTY)),
+		tap(console.log),
+		tap(addUsers)
+	)
+}
+
+const handlePrev = createEffect((actions) => {
+	return actions.pipe(
+		ofType(fetchPrevAction),
+    tap(console.log),
+		switchMap(() =>
+    combineLatest([meta$, hasPrev$]).pipe(
+        take(1),
+				switchMap(([meta, hasNext]) => iif(() => hasNext, from(getUsersPage(meta.page - 1)), EMPTY)),
+			)
+		),
+    tap(addUsers),
+	)
+})
+
+const handleNext = createEffect((actions) => {
+	return actions.pipe(
+		ofType(fetchNextAction),
+    tap(console.log),
+		switchMap(() =>
+      combineLatest([meta$, hasNext$]).pipe(
+        take(1),
+        switchMap(([meta, hasNext]) => iif(() => hasNext, from(getUsersPage(meta.page + 1)), EMPTY)),
+      )
+		),
+    tap(addUsers),
+	)
+})
+
+registerEffects([handlePrev, handleNext])
