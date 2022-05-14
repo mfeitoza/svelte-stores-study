ee19ae14bcf7de6041d10cffbdd7ec7906091d7e
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2022-05-14 02:01:10 -0300

    feat(routes): add boilerplate

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,23 +5,45 @@
 	<title>Stores Study</title>
 </svelte:head>
 
-<div class="mx-auto flex justify-center py-24 space-x-4">
-	<a href="/nanostore" class="bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-indigo-700">
-		nanostore
-	</a>
-	<a href="/storeon" class="bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-indigo-700">
-		storeon
-	</a>
-	<a href="/zustand" class="bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-indigo-700">
-		zustand
-	</a>
-	<a href="/valtio" class="bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-indigo-700">
-		valtio
-	</a>
-	<a href="/custom" class="bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-indigo-700">
-		custom
-	</a>
-	<a href="/effector" class="bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-indigo-700">
-		effector
-	</a>
-</div>
+<section class="pt-24">
+	<h1 class="text-indigo-600 font-semibold text-center text-xl mb-4">
+		Unopinionated state management
+	</h1>
+	<div class="mx-auto flex justify-center space-x-4">
+		<a href="/nanostore" class="bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-indigo-700">
+			nanostore
+		</a>
+		<a href="/storeon" class="bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-indigo-700">
+			storeon
+		</a>
+		<a href="/zustand" class="bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-indigo-700">
+			zustand
+		</a>
+		<a href="/valtio" class="bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-indigo-700">
+			valtio
+		</a>
+		<a href="/custom" class="bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-indigo-700">
+			custom
+		</a>
+		<a href="/effector" class="bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-indigo-700">
+			effector
+		</a>
+	</div>
+</section>
+
+<section class="pt-10">
+	<h1 class="text-orange-600 font-semibold text-center text-xl mb-4">
+		Opinionated state management
+	</h1>
+	<div class="mx-auto flex justify-center space-x-4">
+		<a href="/elf" class="bg-orange-500 text-white px-4 py-2 rounded-md hover:bg-orange-700">
+			elf
+		</a>
+		<a href="/mobx" class="bg-orange-500 text-white px-4 py-2 rounded-md hover:bg-orange-700">
+			mobx
+		</a>
+		<a href="/overmind" class="bg-orange-500 text-white px-4 py-2 rounded-md hover:bg-orange-700">
+			overmind
+		</a>
+	</div>
+</section>
---
+++ src/routes/test.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Zustand from '$lib/examples/zustand.svelte'
+	import Valtio from '$lib/examples/valtio.svelte'
+	import Nano from '$lib/examples/nanostore.svelte'
+</script>
+
+<svelte:head>
+	<title>teset</title>
+</svelte:head>
+
+<div class="flex space-x-8">
+	<Zustand />
+	<Valtio />
+	<Nano />
+</div>
